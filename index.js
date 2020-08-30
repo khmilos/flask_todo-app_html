203a4a9393aab3066db87bb5954b49085712f959
khmilos <khmil.oleksandr32@gmail.com>
2020-08-30 11:20:03 +0300

    init

---
+++ index.js
@@ -0,0 +1,193 @@
+(function() {
+  const body = document.querySelector('.js-body');
+  // Modal Shadow
+  const shadow = document.createElement('DIV');
+  shadow.classList.add('modal-shadow');
+
+  // Constants
+  const MIN_TIME = 11;
+  const ANIMATION_TIME = 500;
+
+  // Functions
+  function sleep(ms) { 
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  // Classes
+  class Modal {
+    constructor({ container, body, shadow, }, time) {
+      this.container = container;
+      this.body = body;
+      this.shadow = shadow;
+      this.time = time;
+      this.status = {
+        isOpen: false,
+        isAnimated: false,
+      };
+    }
+  
+    async open() {
+      if (this.status.isAnimated || this.status.isOpen) return this;
+      this.status.isAnimated = true;
+  
+      this.container.parentNode.classList.add('forward');
+      this.body.classList.add('body-fixed');
+      this.body.appendChild(shadow);
+      await sleep(MIN_TIME);
+      this.container.parentNode.classList.add('active');
+      this.shadow.classList.add('active');
+      await sleep(this.time);
+  
+      this.status.isOpen = true;
+      this.status.isAnimated = false;
+      return this;
+    }
+  
+    async close() {
+      if (this.status.isAnimated || !this.status.isOpen) return this;
+      this.status.isAnimated = true;
+  
+      this.shadow.classList.remove('active');
+      this.container.parentNode.classList.remove('active');
+      await sleep(this.time);
+      this.container.parentNode.classList.remove('forward');
+      this.body.removeChild(this.shadow);
+      this.body.classList.remove('body-fixed');
+  
+      this.status.isOpen = false;
+      this.status.isAnimated = false;
+      return this;
+    }
+    
+    toggle() {
+      if (!this.status.isOpen) this.open();
+      else this.close();
+    }
+  }
+
+  class Header extends Modal {
+    constructor({ container, body, shadow, dropdown, burger }, time) {
+      super({ container, body, shadow, }, time);
+      this.dropdown = dropdown;
+      this.burger = burger;
+    }
+
+    async open() {
+      if (this.status.isAnimated || this.status.isOpen) return this;
+      this.status.isAnimated = true;
+
+      this.body.classList.add('body-fixed');
+      this.burger.classList.add('active');
+      this.dropdown.classList.add('active');
+      this.container.classList.add('forward');
+      const links = document.querySelectorAll('.js-nav__item');
+      let height = 0;
+      links.forEach((element) => height += element.offsetHeight);
+      this.dropdown.setAttribute('style', `height: ${height}px;`);
+      this.body.appendChild(this.shadow);
+      await sleep(MIN_TIME);
+      this.shadow.classList.add('active');
+      await sleep(ANIMATION_TIME);
+
+      this.status.isOpen = true;
+      this.status.isAnimated = false;
+      return this;
+    }
+
+    async close() {
+      if (this.status.isAnimated || !this.status.isOpen) return false;
+      this.status.isAnimated = this;
+  
+      this.burger.classList.remove('active');
+      this.dropdown.classList.remove('active');
+      this.dropdown.setAttribute('style', '');
+      this.shadow.classList.remove('active');
+      await sleep(ANIMATION_TIME);
+      body.removeChild(this.shadow);
+      this.container.classList.remove('forward');
+      this.body.classList.remove('body-fixed');
+  
+      this.status.isOpen = false;
+      this.status.isAnimated = false;
+      return this;
+    }
+  }
+
+  // Factories
+  class modalFactory {
+    static _subscribe({ toOpen, toClose, toToggle }, modal, shadow) {
+      const action = [modal.open, modal.close, modal.toggle];
+      [toOpen, toClose, toToggle].forEach((group, index) => {
+        if (!group) return;
+        group.forEach((element) => {
+          element.addEventListener('click', (e) => {
+            e.preventDefault();
+            action[index].call(modal);
+          });
+        });
+      });
+      window.addEventListener('click', (event) => {
+        if (event.target === modal.container.parentNode
+          || event.target === shadow) {
+            modal.close();
+          }
+      });
+      return modal;
+    }
+
+    static createModal(modalElements, animation, triggerElements) {
+      if (!modalElements.container) return null;
+      return this._subscribe(
+        triggerElements, 
+        new Modal(modalElements, animation),
+        modalElements.shadow,
+      );
+    }
+
+    static createHeader(modalElements, animation, triggerElements) {
+      if (!modalElements.container) return null;
+      return this._subscribe(
+        triggerElements,
+        new Header(modalElements, animation),
+        modalElements.shadow,
+      );
+    }
+  }
+  
+  // Scripts
+  const header = modalFactory.createHeader(
+    {
+      container: document.querySelector('.js-header'),
+      dropdown: document.querySelector('.js-nav'),
+      burger: document.querySelector('.js-burger'),
+      body,
+      shadow,
+    },
+    ANIMATION_TIME,
+    { toToggle: [document.querySelector('.js-burger')] },
+  );
+
+  const profile = modalFactory.createModal(
+    {
+      container: document.querySelector('.js-profile-setup'),
+      body,
+      shadow,
+    },
+    ANIMATION_TIME, {
+      toOpen: [document.querySelector('.js-customize')],
+      toClose: [document.querySelector('.js-profile-setup-close')],
+    },
+  );
+
+  const filter = modalFactory.createModal(
+    {
+      container: document.querySelector('.js-filter-boards'),
+      body,
+      shadow,
+    }, ANIMATION_TIME,
+    { 
+      toOpen: [document.querySelector('.js-filter-boards-btn')],
+      toClose: [document.querySelector('.js-filter-boards-close')],
+    }
+  );
+})()
